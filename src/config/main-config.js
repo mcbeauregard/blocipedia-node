218298054a6b96059eac88f2ef0719662887fa3d
mcbeauregard <mcbeauregard@gmail.com>
2018-12-03 13:37:37 -0800

    user sign-up in progress

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -3,6 +3,10 @@ const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
 const logger = require('morgan');
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+const passportConfig = require("./passport-config");
 
 module.exports = {
     init(app, express){
@@ -11,5 +15,19 @@ module.exports = {
         app.use(express.static(path.join(__dirname, "..", "assets")));
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(logger('dev'));
-  }
+        app.use(expressValidator());
+        app.use(session({
+            secret: process.env.cookieSecret,
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
+          }));
+          app.use(flash());
+          passportConfig.init(app);
+
+        app.use((req,res,next) => {
+            res.locals.currentUser = req.user;
+            next();
+        })
+        }
 };
---
+++ src/db/queries.users.js
@@ -0,0 +1,34 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports = {
+  createUser(newUser, callback){
+
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+    return User.create({
+      username: newUser.username,
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) => {
+        const msg = {
+        to: newUser.email,
+        from: 'testUser@example.com',
+        subject: 'User Confirmation',
+        text: 'Welcome to Blocipedia!',
+        html: '<strong>Please login to your account to confirm membership!</strong>',
+      };
+      sgMail.send(msg);
+      callback(null, user);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+
+}
