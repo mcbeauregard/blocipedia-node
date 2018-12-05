31be78dbbed6531c7a5ce340405f0bf7227011fc
mcbeauregard <mcbeauregard@gmail.com>
2018-12-05 15:16:16 -0800

    debugging

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,8 +1,8 @@
 const User = require("./models").User;
 const bcrypt = require("bcryptjs");
-//const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail');
 
-//sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   createUser(newUser, callback){
@@ -16,14 +16,14 @@ module.exports = {
       password: hashedPassword
     })
     .then((user) => {
-        //const msg = {
-        //to: newUser.email,
-        //from: 'testUser@example.com',
-        //subject: 'User Confirmation',
-        //text: 'Welcome to Blocipedia!',
-       // html: '<strong>Please login to your account to confirm membership!</strong>',
-     // };
-     // sgMail.send(msg);
+        const msg = {
+        to: newUser.email,
+        from: 'testUser@example.com',
+        subject: 'User Confirmation',
+        text: 'Welcome to Blocipedia!',
+       html: '<strong>Please login to your account to confirm membership!</strong>',
+      };
+      sgMail.send(msg);
       callback(null, user);
     })
     .catch((err) => {
