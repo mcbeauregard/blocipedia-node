ef89717df5f03a17e39b7257d2e66d5c56e03c44
mcbeauregard <mcbeauregard@gmail.com>
2018-12-07 16:54:42 -0800

    debuggin

--- src/db/migrations/20181207193903-create-wiki.js
+++ src/db/migrations/20181207193903-create-wiki.js
@@ -25,16 +25,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
-      userId: {
-        type: Sequelize.INTEGER,
-        onDelete: "CASCADE",
-        allowNull: false,
-        references: {
-          model: "Users",
-          key: "id",
-          as: "userId",
-        },
-      },
     });
   },
   down: (queryInterface, Sequelize) => {
