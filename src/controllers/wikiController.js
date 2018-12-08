488186429adce16166e3091e851c6d4ca81e051b
mcbeauregard <mcbeauregard@gmail.com>
2018-12-07 18:01:46 -0800

    wiki crud view working

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,16 +1,16 @@
 const wikiQueries = require("../db/queries.wikis.js");
 
 module.exports = {
-    index(req, res, next){
-
+  index(req, res, next) {
+    
     wikiQueries.getAllWikis((err, wikis) => {
-        if(err){
-          res.redirect(500, "static/index");
+        if(err){                
+            res.redirect(500, "static/index");
         } else {
-          res.render("wikis/index", {wikis});
+            res.render("wikis/index", {wikis});
         }
-      })
-   },
+    })
+},
 
   new(req, res, next){
     res.render("wikis/new");
