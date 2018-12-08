ab2a16a7b8469cec0b65f844fd4eb9fa474735b3
mcbeauregard <mcbeauregard@gmail.com>
2018-12-08 11:10:08 -0800

    wiki CRUD DONE

--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -4,7 +4,15 @@ module.exports = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     body: DataTypes.STRING,
     private: DataTypes.BOOLEAN,
-    userId: DataTypes.INTEGER
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+        as: "userId"
+      }
+    },
   }, {});
   Wiki.associate = function(models) {
     // associations can be defined here
