49fc13d62c6ef72d737d3a7c48ce323e541da5a4
mcbeauregard <mcbeauregard@gmail.com>
2018-12-16 18:36:39 -0800

    collab in progress

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -22,15 +22,27 @@ module.exports = {
     })
   },
 
-   getUser(id, callback){
-     return User.findById(id)
-     .then((user) => {
-      callback(null, user);
-      })
-         .catch((err) => {
-          callback(err);
-       })
-   },
+  getUser(id, callback) {
+    let result = {};
+    User.findById(id)
+        .then((user) => {
+            if (!user) {
+                callback(404);
+            } else {
+                result["user"] = user;
+                Collaborator.scope({
+                        method: ["userCollaborationsFor", id]
+                    }).all()
+                    .then((collaborations) => {
+                        result["collaborations"] = collaborations;
+                        callback(null, result);
+                    })
+                    .catch((err) => {
+                        callback(err);
+                    })
+            }
+        })
+},
 
    upgradeUserRole(req, callback){
     return User.findById(req.user.id)
