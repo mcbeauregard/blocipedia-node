ee82d57e8cb186e8da1660c75d93c1f3fd3ff743
mcbeauregard <mcbeauregard@gmail.com>
2018-12-18 09:41:58 -0800

    add collaborators functioning

--- .history/src/db/queries.users_20181218092905.js
+++ .history/src/db/queries.users_20181218092905.js
@@ -21,27 +21,15 @@ module.exports = {
     })
   },
 
-  getUser(id, callback) {
-    let result = {};
-    User.findById(id)
-        .then((user) => {
-            if (!user) {
-                callback(404);
-            } else {
-                result["user"] = user;
-                Collaborator.scope({
-                        method: ["userCollaborationsFor", id]
-                    }).all()
-                    .then((collaborations) => {
-                        result["collaborations"] = collaborations;
-                        callback(null, result);
-                    })
-                    .catch((err) => {
-                        callback(err);
-                    })
-            }
-        })
-},
+   getUser(id, callback){
+     return User.findById(id)
+     .then((user) => {
+      callback(null, user);
+      })
+         .catch((err) => {
+          callback(err);
+       })
+   },
 
    upgradeUserRole(req, callback){
     return User.findById(req.user.id)
