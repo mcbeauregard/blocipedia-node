6992a256b617c10a2239a725485a827d2cdd92e2
mcbeauregard <mcbeauregard@gmail.com>
2018-12-19 13:53:25 -0800

    edit view, and delete button working

--- src/routes/collaborators.js
+++ src/routes/collaborators.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const collaboratorController = require("../controllers/collaboratorController");
+const User = require("../../src/db/models").User;
 
-const collaboratorController = require("../controllers/collaboratorController")
+router.get("/wikis/:wikiId/collaborators", collaboratorController.show);
 
-router.get("/wikis/:wikiId/collaborators/:id", collaboratorController.show);
 router.post("/wikis/:wikiId/collaborators/create", collaboratorController.create);
 router.post("/wikis/:wikiId/collaborators/delete", collaboratorController.delete);
 
-module.exports = router;
+module.exports = router; 
